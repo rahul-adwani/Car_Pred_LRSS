86722bcf9f31813c3cf4486daa61ec13dde385a4
alley9515 <61354796+alley9515@users.noreply.github.com>
2020-07-23 15:53:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('lr_model.pkl.pkl', 'rb'))
+model = pickle.load(open('lr_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
